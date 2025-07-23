66955378d125900c9b830db257e19ef9c4598a21
GidiWC <241189694@tut4life.ac.za>
2025-07-23 21:11:58 +0200

    Add files via upload

---
+++ MyFirstFrameApp.java
@@ -0,0 +1,14 @@
+
+package myfirstframeapp;
+
+import wama.tut.ac.MyFirstFrame;
+
+public class MyFirstFrameApp {
+
+    public static void main(String[] args) {
+        MyFirstFrame frame;
+        frame=new MyFirstFrame();
+        
+    }
+    
+}
